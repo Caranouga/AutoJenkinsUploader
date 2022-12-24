fc25318ecbf34407eb451d1e9e802df788ebeee3
Malone <malonepayet@gmail.com>
2022-12-24 21:40:02 +0100

    Initial commit

---
+++ src/main/java/fr/caranouga/autojenkinsuploader/AutoJenkinsUploader.java
@@ -0,0 +1,17 @@
+package fr.caranouga.autojenkinsuploader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoJenkinsUploader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
